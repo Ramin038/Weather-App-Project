f808e30691b4d9d16595ac1fea02de3dc8cecfa6
Ramin038 <71678173+Ramin038@users.noreply.github.com>
2020-11-17 20:45:43 -0500

    Forecast updatied function - currently not pulling time stamp

--- src/script.js
+++ src/script.js
@@ -29,7 +29,15 @@ function currentDate() {
 currentDate();
 
 function formatHours(timestamp) {
-  
+   let date = new Date(timestamp)
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+   if (minutes < 10) {
+    minutes= `0${minutes}`;
+  }
   return `${hours}:${minutes}`;
 }
 
